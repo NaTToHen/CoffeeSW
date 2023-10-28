f1770eeb998bbb330172c02f9e5735c6665c7e8b
Henrique Bonatto <nattohen@gmail.com>
2023-10-28 10:04:34 -0300

    teste

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -20,11 +20,9 @@ function Home() {
    useEffect(() => {
       axios.get('http://localhost:8000/api/db/catalogo')
          .then(response => {
-            // Faça algo com os dados da resposta, por exemplo, definir o estado do componente
             setData(response.data)
          })
          .catch(error => {
-            // Lida com erros, por exemplo, definindo um estado de erro
             setError(error)
          })
    }, [])
